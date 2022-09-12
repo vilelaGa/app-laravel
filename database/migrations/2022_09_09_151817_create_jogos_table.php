23704149007b79cacafc2ee2d2715550ff81d2dd
Gabriel de Souza Lima Vilela <05845@base.sobeu.br>
2022-09-12 17:21:17 -0300

    migrate

--- database/migrations/2022_09_09_151817_create_jogos_table.php
+++ database/migrations/2022_09_09_151817_create_jogos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJogoTable extends Migration
+class CreateJogosTable extends Migration
 {
     /**
      * Run the migrations.
